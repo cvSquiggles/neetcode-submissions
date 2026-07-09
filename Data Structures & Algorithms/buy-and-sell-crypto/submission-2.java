5b699d3d511f71aebd6700b1ec3333854e689daf
cvSquiggz <47958401+cvSquiggles@users.noreply.github.com>
2026-07-08 21:13:29 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for(int i = 0; i < prices.length; i++)
+        {
+            for(int j = i + 1; j < prices.length; j++)
+            {
+                int potentialProfit = prices[j] - prices[i];
+                if(potentialProfit > profit)
+                {
+                    profit = potentialProfit;
+                }
+            }
+        }
+
+        return profit;
+    }
+}
